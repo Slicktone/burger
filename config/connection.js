f8ea5d16887343fd8ad76309a8ef63ad1b102bc6
TonyUCLA <TonyUCLA>
2017-01-15 15:13:28 -0800

    connection to SQL created

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+// set up SQL and make connection here
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    port: 3000,
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error("Something happened while connecting: " + err.stack);
+        return;
+    }
+    console.log("Connected as id " + connection.threadId);
+});
+
+// exporting connection for the ORM
+module.exports = connection;
