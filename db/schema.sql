ab4c330701b4e3105189fff7d9d31ee4e3eea3d0
TonyUCLA <TonyUCLA>
2017-01-15 14:12:45 -0800

    timstamp and table name changes

--- db/schema.sql
+++ db/schema.sql
@@ -2,10 +2,10 @@ CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
-CREATE TABLE burgers_db (
+CREATE TABLE burgers (
     id int NOT NULL AUTO_INCREMENT,
     burger_name varchar(100) NOT NULL,
     devoured BOOLEAN DEFAULT false,
-    date timestamp NOT NULL DEFAULT NOW(),
+    date TIMESTAMP NOT NULL DEFAULT NOW(),
     PRIMARY KEY (id)
 );
