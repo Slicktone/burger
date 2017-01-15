82493c092bae01661d5396b477c78472c003b6ac
TonyUCLA <TonyUCLA>
2017-01-15 15:13:47 -0800

    Dummy seed values inserted

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+
