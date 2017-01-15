d0951173d6a44e809b4f4471f95ee34dd8c39c52
TonyUCLA <TonyUCLA>
2017-01-15 14:10:36 -0800

    timestamp added

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,7 @@ USE burgers_db;
 CREATE TABLE burgers_db (
     id int NOT NULL AUTO_INCREMENT,
     burger_name varchar(100) NOT NULL,
-    devoured BOOLEAN DEFAULT false
+    devoured BOOLEAN DEFAULT false,
+    date timestamp NOT NULL DEFAULT NOW(),
     PRIMARY KEY (id)
 );
