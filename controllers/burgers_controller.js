d3b10260edfabf36fb3419ff773d06ed0737c717
TonyUCLA <TonyUCLA>
2017-03-31 12:25:44 -0700

    controller routes updated

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,44 +1,39 @@
 var express = require('express');
 // using the burger.js database functions
 var burger = require('../models/burger.js');
-
 // routes will be created here and exported
 var router = express.Router();
 
-router.get('/', function(request, response) {
-    burger.selectAll(function(burgers) {
-        // var nonDevoured = burgers.filter(function(b) {
-        //     return b.devoured === false;
-        // })
-
-        // var devoured = burgers.filter(function(b) {
-        //     return b.devoured === true;
-        // })
-
-        var hbsObject = {
-            burger: burgers
-        };
-        console.log(hbsObject);
-        response.render('index', hbsObject);
-    });
+router.get('/', function(req, res) {
+    res.redirect('/burgers');
 });
 
-router.post('/', function(request, response) {
-    var col = ["burger_name"];
-    var val = [req.body.burger_name];
-    burger.insertOne(col, val, function() {
-        response.redirect('/');
+router.get('/burgers', function(req, resp) {
+    // express callback response by calling burger.selectAll
+    burger.selectAll(function(burgerData) {
+// wrapper for orm.js that using MySQL query callback will return burger_data,
+// render to index with handlebar
+        res.render('index', { burger_data: burgerData });
     });
 });
+    
+// post route ---> back to index
+router.post('/burgers/create', function(req, res) {
+    // takes the request object using it as input for burger.addBurger
+    burger.create(req.body.burger_name, function(result) {
+    // wrapper for orm.js that using MySQL insert callback will return a log to console,
+    // render back to index with handle
+        console.log(result);
+        res.redirect("/");
+    })
+});
 
-router.put('/:id', function(request, response) {
-    var condition = 'id = ' + request.params.id;
-    console.log('condition', condition);
-// need objects here for request.body
-    burger.updateOne({
-    bar: request.body.bar
-    }, condition, function() {
-        response.redirect('/');
+// put route ---> back to index
+route.put('/burgers/update', function(req, res) {
+    burger.update(req.body.burger_id, function(result) {
+        // orm.js wrapper thats using SQL to update and return a console log respectively
+        console.log(result);
+        res.redirect("/");
     });
 });
 
